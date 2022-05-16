2726dff765250e25343c8a78f6d117936db2f11d
saddammdjp <105309734+saddammdjp@users.noreply.github.com>
2022-05-16 11:59:29 +0530

    added random comment

--- src/main/java/TechMtest/TechM/GreetingController.java
+++ src/main/java/TechMtest/TechM/GreetingController.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
-
+//Random comment for conflict
     private static final String template = "Hello, %s!";
 
     private final AtomicLong counter = new AtomicLong();
